967fd78e9d6c1178c169e723b5764a2c76a55dde
xvhjs2 <tramtram4400@gmail.com>
2025-05-30 07:20:37 -0400

    Update info.py

--- stuff/info.py
+++ stuff/info.py
@@ -1,11 +1,19 @@
 import requests
 import json
+from datetime import datetime
+
+def date(id):
+    de = 1420070400000
+    time = ((int(id) >> 22) + de) / 1000
+    date = datetime.utcfromtimestamp(time)
+    return date.strftime('%Y-%m-%d %H:%M:%S') + ' ' + 'UTC Timezone' 
 
 headers = {"Content-Type": "application/json"}
 
 def hookinfo(hookurl):
     info = requests.get(hookurl, headers=headers)
     if info.status_code == 200:
-        hookinf = info.json()        
-        hookpfp = f"https://cdn.discordapp.com/avatars/{hookinf['id']}/{hookinf['avatar']}"
-        print(f"Webhook Name: {hookinf['name']}\nWebhook ID: {hookinf['id']}\nWebhook PFP: {hookpfp}\nChannel ID: {hookinf['channel_id']}\nServer ID: {hookinf['guild_id']}\nWebhook Type: {hookinf['type']}\nWebhook Token: {hookinf['token']}")
+        hookinf = info.json()
+        av = {hookinf['avatar']}
+        hookpfp = f"https://cdn.discordapp.com/avatars/{hookinf['id']}/{av}" if av else 'None'
+        print(f"[!] Webhook Name: {hookinf['name']}\n[!] Webhook ID: {hookinf['id']}\n[!] Webhook Creation Date: {date(hookinf['id'])}\n[!] Webhook PFP: {hookpfp}\n[!] Channel ID: {hookinf['channel_id']}\n[!] Server ID: {hookinf['guild_id']}\n[!] Webhook Type: {hookinf['type']}\n[!] Webhook Token: {hookinf['token']}")

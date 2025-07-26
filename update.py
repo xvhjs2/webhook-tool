2f42825fc7b7f7c3fda4b34c10dea9acad9be353
xvhjs2 <tramtram4400@gmail.com>
2025-07-26 18:08:45 -0400

    Create update.py

---
+++ update.py
@@ -0,0 +1,20 @@
+import requests
+import os
+
+def update(file, content):
+    print(f'Updating file {file}')
+    with open(file, 'w') as f:
+        f.write(content)
+        print(f'Successfully updated {file}')
+        
+def recieve():
+    base = "https://raw.githubusercontent.com/xvhjs2/webhook-tool/refs/heads/main/"
+    files = ['main.py', 'stuff/deleter.py', 'stuff/editor.py', 'stuff/info.py', 'stuff/message.py']
+    for fs in files:
+        n = requests.get(f'https://github.com/xvhjs2/webhook-tool/raw/refs/heads/main/{fs}')
+        update(fs, n.text)
+
+recieve()
+
+print('Finished updating all files')
+os.system('pause')

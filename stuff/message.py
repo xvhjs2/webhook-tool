79ad4c3cb3adfc2e32f59b31e2b28258ccbf7005
xvhjs2 <tramtram4400@gmail.com>
2025-07-28 16:36:17 -0400

    Update message.py

--- stuff/message.py
+++ stuff/message.py
@@ -7,7 +7,7 @@ headers = {"Content-Type": "application/json"}
 def sendmsg(hookurl, message):
     payload = {"content": message}
     msg = requests.post(hookurl, headers=headers, json=payload)
-    if msg.status_code == 204:
+    if msg.status_code in [200, 201, 204]:
         print('[+] Sent Message')
     else:
         print(f'[-] Failed To Send Message: {msg.status_code}')
@@ -16,3 +16,19 @@ def spammsg(hookurl, message, amount, cooldown):
     for _ in range(amount):
         sendmsg(hookurl, message)
         time.sleep(cooldown)
+
+def ghostmsg(hookurl, message):
+    payload = {"content": message}
+    hookurl2 = hookurl + '?wait=true'
+    msg = requests.post(hookurl2, headers=headers, json=payload)
+    if msg.status_code in [200, 201, 204]:
+        print('[+] Sent Message')
+        mj = msg.json()
+        msgid = mj['id']
+        md = requests.delete(f'{hookurl}/messages/{msgid}', headers=headers)
+        if md.status_code in [200, 201, 204]:
+            print('[+] Successfully Ghost Messaged')
+        else:
+            print(f'[-] Failed to delete message: {md.status_code}')
+    else:
+        print(f'[-] Failed To Send Message: {msg.status_code}')

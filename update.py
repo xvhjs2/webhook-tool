785dcb3cf2f2f2c73f5f59614bf572d45de67fb9
xvhjs2 <tramtram4400@gmail.com>
2025-07-28 18:55:40 -0400

    Update update.py

--- update.py
+++ update.py
@@ -3,7 +3,7 @@ import os
 
 def update(file, content):
     print(f'Updating file {file}')
-    with open(file, 'w') as f:
+    with open(file, 'w', encoding='utf-8') as f:
         f.write(content)
         print(f'Successfully updated {file}')
         

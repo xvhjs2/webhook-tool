e8bc0c4da2965dd7bc1d85c386fa7cbf1ddeb646
xvhjs2 <tramtram4400@gmail.com>
2025-07-28 18:56:00 -0400

    Update update.py

--- update.py
+++ update.py
@@ -9,7 +9,7 @@ def update(file, content):
         
 def recieve():
     base = "https://raw.githubusercontent.com/xvhjs2/webhook-tool/refs/heads/main/"
-    files = ['main.py', 'stuff/deleter.py', 'stuff/editor.py', 'stuff/info.py', 'stuff/message.py']
+    files = ['main.py', 'update.py', 'stuff/deleter.py', 'stuff/editor.py', 'stuff/info.py', 'stuff/message.py']
     for fs in files:
         n = requests.get(f'https://github.com/xvhjs2/webhook-tool/raw/refs/heads/main/{fs}')
         update(fs, n.text)

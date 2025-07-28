c62ae4240f0976934a85a85c805b0bab063c9d47
xvhjs2 <tramtram4400@gmail.com>
2025-07-28 18:57:19 -0400

    Update update.py

--- update.py
+++ update.py
@@ -4,8 +4,11 @@ import os
 def update(file, content):
     print(f'Updating file {file}')
     with open(file, 'w', encoding='utf-8') as f:
-        f.write(content)
-        print(f'Successfully updated {file}')
+        try:
+            f.write(content)
+            print(f'Successfully updated {file}')
+        except:
+            print(f'failed to update {file}')
         
 def recieve():
     files = ['main.py', 'update.py', 'stuff/deleter.py', 'stuff/editor.py', 'stuff/info.py', 'stuff/message.py']
